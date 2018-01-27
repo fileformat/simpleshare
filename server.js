758fedbd00196314684d40d8676e53751fcd8a18
Andrew Marcuse <fileformat@gmail.com>
2018-01-27 10:26:09 -0500

    reformat code

--- server.js
+++ server.js
@@ -13,49 +13,49 @@ var app = express();
 app.use(express.static('public'));
 
 function getStatus() {
-	var retVal = {};
+    var retVal = {};
 
-	retVal["success"] = true;
-	retVal["message"] = "OK";
-	retVal["timestamp"] = new Date().toISOString();
-	retVal["__dirname"] = __dirname;
-	retVal["__filename"] = __filename;
-	retVal["os.hostname"] = os.hostname();
-	retVal["os.type"] = os.type();
-	retVal["os.platform"] = os.platform();
-	retVal["os.arch"] = os.arch();
-	retVal["os.release"] = os.release();
-	retVal["os.uptime"] = os.uptime();
-	retVal["os.loadavg"] = os.loadavg();
-	retVal["os.totalmem"] = os.totalmem();
-	retVal["os.freemem"] = os.freemem();
-	retVal["os.cpus.length"] = os.cpus().length;
-	// too much junk: retVal["os.networkInterfaces"] = os.networkInterfaces();
-	
-	retVal["process.arch"] = process.arch;
-	retVal["process.cwd"] = process.cwd();
-	retVal["process.execPath"] = process.execPath;
-	retVal["process.memoryUsage"] = process.memoryUsage();
-	retVal["process.platform"] = process.platform;
-	retVal["process.release"] = process.release;
-  retVal["process.title"] = process.title;
-	retVal["process.uptime"] = process.uptime;
-	retVal["process.version"] = process.version;
-	retVal["process.versions"] = process.versions;
-	retVal["process.installPrefix"] = process.installPrefix;
-	
-	return retVal;
+    retVal["success"] = true;
+    retVal["message"] = "OK";
+    retVal["timestamp"] = new Date().toISOString();
+    retVal["__dirname"] = __dirname;
+    retVal["__filename"] = __filename;
+    retVal["os.hostname"] = os.hostname();
+    retVal["os.type"] = os.type();
+    retVal["os.platform"] = os.platform();
+    retVal["os.arch"] = os.arch();
+    retVal["os.release"] = os.release();
+    retVal["os.uptime"] = os.uptime();
+    retVal["os.loadavg"] = os.loadavg();
+    retVal["os.totalmem"] = os.totalmem();
+    retVal["os.freemem"] = os.freemem();
+    retVal["os.cpus.length"] = os.cpus().length;
+    // too much junk: retVal["os.networkInterfaces"] = os.networkInterfaces();
+
+    retVal["process.arch"] = process.arch;
+    retVal["process.cwd"] = process.cwd();
+    retVal["process.execPath"] = process.execPath;
+    retVal["process.memoryUsage"] = process.memoryUsage();
+    retVal["process.platform"] = process.platform;
+    retVal["process.release"] = process.release;
+    retVal["process.title"] = process.title;
+    retVal["process.uptime"] = process.uptime;
+    retVal["process.version"] = process.version;
+    retVal["process.versions"] = process.versions;
+    retVal["process.installPrefix"] = process.installPrefix;
+
+    return retVal;
 }
 
-app.get('/status.json', function(req, res) {
-  res.writeHead(200, {
+app.get('/status.json', function (req, res) {
+    res.writeHead(200, {
         "Content-Type": "text/plain",
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, GET',
         'Access-Control-Max-Age': '604800'
-      });
+    });
 
-  sendJson(req, res, getStatus());
+    sendJson(req, res, getStatus());
 });
 
 function getVariables(req, res) {
@@ -67,12 +67,12 @@ function getVariables(req, res) {
         sendJson(req, res, result);
         return null;
     }
-  
+
     var text = req.query["text"];
-	if (text == null) {
+    if (text == null) {
         text = req.query["url"];      // LATER: split off just filename?
     }
-  
+
     var summary = req.query["summary"];
     if (summary == null) {
         summary = "";
@@ -92,53 +92,51 @@ function getVariables(req, res) {
 }
 
 function getSite(req, res) {
-  var result = {};
-  if ('site' in req.query == false) {
-    result["success"] = false;
-    result["code"] = 400;
-    result["message"] = "Parameter 'site' is required";
-    sendJson(req, res, result);
-    return null;
-  }
-  
-  var site = req.query["site"];
-	if (share_urls[site] == null) {
-    result["success"] = false;
-    result["code"] = 404;
-    result["message"] = "Site '" + site + "' is not supported yet";
-    sendJson(req, res, result);
-    return null;
-  }
+    var result = {};
+    if ('site' in req.query == false) {
+        result["success"] = false;
+        result["code"] = 400;
+        result["message"] = "Parameter 'site' is required";
+        sendJson(req, res, result);
+        return null;
+    }
 
-  return site;
+    var site = req.query["site"];
+    if (share_urls[site] == null) {
+        result["success"] = false;
+        result["code"] = 404;
+        result["message"] = "Site '" + site + "' is not supported yet";
+        sendJson(req, res, result);
+        return null;
+    }
+
+    return site;
 }
 
 function make_template(strings, ...keys) {
-  return (function(...values) {
-    var dict = values[values.length - 1] || {};
-    var result = [strings[0]];
-    keys.forEach(function(key, i) {
-      var value = Number.isInteger(key) ? values[key] : dict[key];
-      result.push(value, strings[i + 1]);
+    return (function (...values) {
+        var dict = values[values.length - 1] || {};
+        var result = [strings[0]];
+        keys.forEach(function (key, i) {
+            var value = Number.isInteger(key) ? values[key] : dict[key];
+            result.push(value, strings[i + 1]);
+        });
+        return result.join('');
     });
-    return result.join('');
-  });
 }
 
 
 function sendJson(req, res, jsonObj) {
-	if ('callback' in req.query)
-	{
-		res.write(req.query["callback"]);
-		res.write("(");
-		res.write(JSON.stringify(jsonObj));
-		res.write(");");
-	}
-	else
-	{
-		res.write(JSON.stringify(jsonObj));
-	}
-  res.end();
+    if ('callback' in req.query) {
+        res.write(req.query["callback"]);
+        res.write("(");
+        res.write(JSON.stringify(jsonObj));
+        res.write(");");
+    }
+    else {
+        res.write(JSON.stringify(jsonObj));
+    }
+    res.end();
 }
 
 function trackEvent(req, ga_id, event) {
@@ -162,88 +160,96 @@ function trackEvent(req, ga_id, event) {
     formData.tid = ga_id;
     formData.cid = '';    // anonymous client id
     formData.t = 'event';
-    if (event.eventCategory) { formData.ec = event.eventCategory; }
-    if (event.eventAction) { formData.ea = event.eventAction; }
-    if (event.eventLabel) { formData.el = event.eventLabel; }
-    if (event.eventValue) { formData.ev = event.eventValue; }
+    if (event.eventCategory) {
+        formData.ec = event.eventCategory;
+    }
+    if (event.eventAction) {
+        formData.ea = event.eventAction;
+    }
+    if (event.eventLabel) {
+        formData.el = event.eventLabel;
+    }
+    if (event.eventValue) {
+        formData.ev = event.eventValue;
+    }
     formData.ua = req.get('user-agent') || '(not set)';
     formData.uip = req.ip || '0.0.0.0';
 
     request.post({
         url: "https://www.google-analytics.com/collect",
         formData: formData
-    }, function (error, response, body){
+    }, function (error, response, body) {
         console.log("INFO: ga tracking returned " + response.statusCode);
     });
 }
 
-app.get('/sitelist.json', function(req, res) {
-  var result = {};
-  
-  var sites = []; 
-  
-  var keys = Object.keys(share_urls);
-  for (var loop = 0; loop < keys.length; loop++) {
-    var site = share_urls[keys[loop]];
-    var site_result = { name: site.name, id: keys[loop] };
-    if (site.logo) { 
-     site_result.logolink = "https://www.vectorlogo.zone/logos/" + site.logo + "/index.html";
-     site_result.logo = "https://www.vectorlogo.zone/logos/" + site.logo + "/" + site.logo + "-tile.svg";
+app.get('/sitelist.json', function (req, res) {
+    var result = {};
+
+    var sites = [];
+
+    var keys = Object.keys(share_urls);
+    for (var loop = 0; loop < keys.length; loop++) {
+        var site = share_urls[keys[loop]];
+        var site_result = {name: site.name, id: keys[loop]};
+        if (site.logo) {
+            site_result.logolink = "https://www.vectorlogo.zone/logos/" + site.logo + "/index.html";
+            site_result.logo = "https://www.vectorlogo.zone/logos/" + site.logo + "/" + site.logo + "-tile.svg";
+        }
+        sites.push(site_result);
     }
-    sites.push(site_result);
-  }
-  
-  result["sites"] = sites;
-  result["success"] = true;
-  result["code"] = 0;
-  result["message"] = result["sites"].length + " sites available";
-  sendJson(req, res, result);
+
+    result["sites"] = sites;
+    result["success"] = true;
+    result["code"] = 0;
+    result["message"] = result["sites"].length + " sites available";
+    sendJson(req, res, result);
 });
 
-app.get('/siteinfo.json', function(req, res) {
-  var site = getSite(req, res);
-  if (site == null) {
-    return;
-  }
-  
-  var result = {};
-  
-  result["success"] = true;
-  result["message"] = "Information for " + share_urls[site].name;
-  result["code"] = 0;
-  result["site"] = site;
-  result["info"] = { "name": share_urls[site].name, "url": share_urls[site].url_template };
-  
-  if (share_urls[site].logo) {
-    result.info.logo = "https://www.vectorlogo.zone/logos/" + share_urls[site].logo + "/" + share_urls[site].logo + "-tile.svg";
-  }
-  sendJson(req, res, result);
+app.get('/siteinfo.json', function (req, res) {
+    var site = getSite(req, res);
+    if (site == null) {
+        return;
+    }
+
+    var result = {};
+
+    result["success"] = true;
+    result["message"] = "Information for " + share_urls[site].name;
+    result["code"] = 0;
+    result["site"] = site;
+    result["info"] = {"name": share_urls[site].name, "url": share_urls[site].url_template};
+
+    if (share_urls[site].logo) {
+        result.info.logo = "https://www.vectorlogo.zone/logos/" + share_urls[site].logo + "/" + share_urls[site].logo + "-tile.svg";
+    }
+    sendJson(req, res, result);
 });
 
-app.get('/siteurl.json', function(req, res) {
-  var site = getSite(req, res);
-  if (site == null) {
-    return;
-  }
-  
-  var vars = getVariables(req, res);
-  if (vars == null) {
-    return;
-  }
-  
-  var result = {};
-  
-  result["success"] = true;
-  result["message"] = "Link to '" + req.query["url"] + "' for " + share_urls[site].name;
-  result["code"] = 0;
-  result["site"] = site;
-  result["url"] = share_urls[site].url_template( vars );
-  
-  sendJson(req, res, result);
+app.get('/siteurl.json', function (req, res) {
+    var site = getSite(req, res);
+    if (site == null) {
+        return;
+    }
+
+    var vars = getVariables(req, res);
+    if (vars == null) {
+        return;
+    }
+
+    var result = {};
+
+    result["success"] = true;
+    result["message"] = "Link to '" + req.query["url"] + "' for " + share_urls[site].name;
+    result["code"] = 0;
+    result["site"] = site;
+    result["url"] = share_urls[site].url_template(vars);
+
+    sendJson(req, res, result);
 });
 
 
-app.get('/go', function(req, res) {
+app.get('/go', function (req, res) {
     var site = getSite(req, res);
     if (site == null) {
         return;
@@ -255,19 +261,27 @@ app.get('/go', function(req, res) {
     }
 
 
-    var loc = share_urls[site].url_template( vars );
-  
+    var loc = share_urls[site].url_template(vars);
+
     res.redirect(loc);
 
     //webmaster's tracking
-    trackEvent(req, req.query["ga"], { eventCategory: 'SHARE', eventAction: req.query["site"], eventValue: req.query["url"]});
+    trackEvent(req, req.query["ga"], {
+        eventCategory: 'SHARE',
+        eventAction: req.query["site"],
+        eventValue: req.query["url"]
+    });
 
     //simpleshare tracking
-    trackEvent(req, process.env.GA_ID, { eventCategory: 'SHARE', eventAction: req.query["site"], eventValue: req.query["url"]});
+    trackEvent(req, process.env.GA_ID, {
+        eventCategory: 'SHARE',
+        eventAction: req.query["site"],
+        eventValue: req.query["url"]
+    });
 });
 
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Listening on port ' + listener.address().port);
+    console.log('Listening on port ' + listener.address().port);
 });
 
 // brutal HACK to avoid having to do ${'TEXT'} in the templates
@@ -277,18 +291,66 @@ var SUMMARY = 'SUMMARY';
 var IMAGE = 'IMAGE';
 
 var share_urls = {
-    'facebook': { name: 'Facebook', logo: 'facebook', url_template: make_template`https://facebook.com/sharer/sharer.php?u=${URL}`},
-    'googleplus': { name: 'Google+', logo: 'google_plus', url_template: make_template`https://plus.google.com/share?url=${URL}`},
-    'linkedin': { name: 'LinkedIn', logo: 'linkedin', url_template: make_template`https://www.linkedin.com/shareArticle?mini=true&url=${URL}&title=${TEXT}&summary=${TEXT}&source=${URL}`},
-    'pinboard': { name: 'Pinboard', logo: 'pinboard', url_template: make_template`https://pinboard.in/add?next=same&url=${URL}&description=${SUMMARY}&title=${TEXT}`},
-    'pinterest': { name: 'Pinterest', logo: 'pinterest', url_template: make_template`https://pinterest.com/pin/create/button/?url=${URL}&media=${IMAGE}&summary=${TEXT}`},
-    'pocket': { name: 'Pocket', logo: 'getpocket', url_template: make_template`https://getpocket.com/save?url=${URL}&title=${TEXT}`},
-    'reddit': { name: 'Reddit', logo: 'reddit', url_template: make_template`https://reddit.com/submit/?url=${URL}`},
-    'tumblr': { name: 'Tumblr', logo: 'tumblr', url_template: make_template`https://www.tumblr.com/widgets/share/tool?posttype=link&title=${TEXT}&caption=${TEXT}&content=${URL}&canonicalUrl=${URL}&shareSource=tumblr_share_button`},
-    'twitter': { name: 'Twitter', logo: 'twitter', url_template: make_template`https://twitter.com/intent/tweet/?text=${TEXT}&url=${URL}`},
-    'whatsapp': { name: 'WhatsApp', logo: 'whatsapp', url_template: make_template`whatsapp://send?text=${TEXT}%20${URL}`},
-    'wordpress': { name: 'Wordpress', logo: 'wordpress', url_template: make_template`https://wordpress.com/press-this.php?u=${URL}&t=${TEXT}&s=${SUMMARY}&i=${IMAGE}`},
-    'xing': { name: 'XING', logo: 'xing', url_template: make_template`https://www.xing.com/app/user?op=share;url=${URL};title=${TEXT}`},
-    'ycombinator': { name: 'Ycombinator (Hacker News)', logo: 'ycombinator', url_template: make_template`https://news.ycombinator.com/submitlink?u=${URL}&t=${TEXT}`}
+    'facebook': {
+        name: 'Facebook',
+        logo: 'facebook',
+        url_template: make_template`https://facebook.com/sharer/sharer.php?u=${URL}`
+    },
+    'googleplus': {
+        name: 'Google+',
+        logo: 'google_plus',
+        url_template: make_template`https://plus.google.com/share?url=${URL}`
+    },
+    'linkedin': {
+        name: 'LinkedIn',
+        logo: 'linkedin',
+        url_template: make_template`https://www.linkedin.com/shareArticle?mini=true&url=${URL}&title=${TEXT}&summary=${TEXT}&source=${URL}`
+    },
+    'pinboard': {
+        name: 'Pinboard',
+        logo: 'pinboard',
+        url_template: make_template`https://pinboard.in/add?next=same&url=${URL}&description=${SUMMARY}&title=${TEXT}`
+    },
+    'pinterest': {
+        name: 'Pinterest',
+        logo: 'pinterest',
+        url_template: make_template`https://pinterest.com/pin/create/button/?url=${URL}&media=${IMAGE}&summary=${TEXT}`
+    },
+    'pocket': {
+        name: 'Pocket',
+        logo: 'getpocket',
+        url_template: make_template`https://getpocket.com/save?url=${URL}&title=${TEXT}`
+    },
+    'reddit': {name: 'Reddit', logo: 'reddit', url_template: make_template`https://reddit.com/submit/?url=${URL}`},
+    'tumblr': {
+        name: 'Tumblr',
+        logo: 'tumblr',
+        url_template: make_template`https://www.tumblr.com/widgets/share/tool?posttype=link&title=${TEXT}&caption=${TEXT}&content=${URL}&canonicalUrl=${URL}&shareSource=tumblr_share_button`
+    },
+    'twitter': {
+        name: 'Twitter',
+        logo: 'twitter',
+        url_template: make_template`https://twitter.com/intent/tweet/?text=${TEXT}&url=${URL}`
+    },
+    'whatsapp': {
+        name: 'WhatsApp',
+        logo: 'whatsapp',
+        url_template: make_template`whatsapp://send?text=${TEXT}%20${URL}`
+    },
+    'wordpress': {
+        name: 'Wordpress',
+        logo: 'wordpress',
+        url_template: make_template`https://wordpress.com/press-this.php?u=${URL}&t=${TEXT}&s=${SUMMARY}&i=${IMAGE}`
+    },
+    'xing': {
+        name: 'XING',
+        logo: 'xing',
+        url_template: make_template`https://www.xing.com/app/user?op=share;url=${URL};title=${TEXT}`
+    },
+    'ycombinator': {
+        name: 'Ycombinator (Hacker News)',
+        logo: 'ycombinator',
+        url_template: make_template`https://news.ycombinator.com/submitlink?u=${URL}&t=${TEXT}`
+    }
 };
 
