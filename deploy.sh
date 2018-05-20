89eeefe343bb1f21b091446d5ffaf5f91cf6f704
Andrew Marcuse <fileformat@gmail.com>
2018-05-19 23:43:15 -0400

    cleanup deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,16 @@
 LASTMOD=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 grep -q "^LASTMOD=" .env
 if [ $? -ne 0 ]; then
-    echo "Appending"
 	echo "LASTMOD=$LASTMOD" >> .env
 else
-    echo "Updating"
 	sed -i -e "s/^LASTMOD=.*$/LASTMOD=$LASTMOD/g" .env
 fi
 
-COMMIT=$(git rev-parse HEAD | cut -c 1-7)
+COMMIT=$(git rev-parse --short HEAD)
 grep -q "^COMMIT=" .env
 if [ $? -ne 0 ]; then
-    echo "Appending"
 	echo "COMMIT=$COMMIT" >> .env
 else
-    echo "Updating"
 	sed -i -e "s/^COMMIT=.*$/COMMIT=$COMMIT/g" .env
 fi
 
